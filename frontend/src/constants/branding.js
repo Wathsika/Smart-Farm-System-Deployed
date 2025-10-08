badac140f01ae59ae908c9980ffe32a747150dfe
Wathsika <182477712+Wathsika@users.noreply.github.com>
2025-10-09 02:05:14 +0530

    Update branding.js

--- frontend/src/constants/branding.js
+++ frontend/src/constants/branding.js
@@ -4,7 +4,7 @@ export const BRAND_DETAILS = Object.freeze({
   address: "10/F, Ginimellagaha, Baddegama, Sri Lanka",
 
   email: "contact@greenleaffarm.com",
-  phone: "+94 77 123 4567",
+  phone: "+94 91 227 6246",
 });
 
 export const BRAND_CONTACT_LINE = `${BRAND_DETAILS.email} | ${BRAND_DETAILS.phone}`;
@@ -15,4 +15,4 @@ export const BRAND_DOCUMENT_TITLES = Object.freeze({
 });
 
 export const formatDocumentTitle = (title) =>
-  (title ? String(title).toUpperCase() : "");
+  title ? String(title).toUpperCase() : "";
